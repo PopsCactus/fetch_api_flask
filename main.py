8e55dca2f46f1c7d1d792569b838f94595e64267
PopsCactus <teo57100@gmail.com>
2021-09-05 17:31:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, json, render_template, jsonify, request
+from flask.helpers import make_response
+
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    return render_template('index.html')
+
+
+@app.route("/api", methods=['GET', 'POST'])
+def geo_code():
+    req = request.get_json()
+
+    print(req)
+    data = datetime.datetime.now()
+
+    res = make_response(jsonify({'Message': data }), 200)
+
+    return res
+
+if '__main__' == __name__:
+    app.run(debug=True)
---
+++ static/js.js
@@ -0,0 +1,28 @@
+var entry = {
+    '1': 'enculer',
+    '2': 'batard'
+};
+
+function enculer() {
+    setInterval(async function(){
+        fetch('/api', {
+            method: 'POST',
+            credentials: "include",
+            body: JSON.stringify(entry),
+            cache: "no-cache",
+            headers: new Headers({"content-type": "application/json"})
+        })
+        .then(function (reponse) {
+        
+            if (reponse.status !== 200){
+                console.log('Reponse au serveur na pas pu etre établie')
+            }
+        
+            reponse.json().then(function (data) {
+                console.log(data.Message)
+                document.getElementById('info').innerText = data
+            })
+        })
+    }, 3000);
+    
+}
